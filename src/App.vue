f45c1836807e928e9705d21f2ed258b102433449
vue-JL <132@njl.com>
2023-12-18 19:37:33 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h3>App组件——测试</h3>
+        <p>哈哈哈哈哈哈哈</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"APP"
+    }
+</script>
+
+<style lang="css" scoped>
+
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,4 @@
+import vue from '@vitejs/plugin-vue2'
+export default {
+    plugins: [vue()]
+}
